ec0bc7b5bc225844fbf3d07154abae5a857160f8
antonystar-dev <antonystar.contatos@gmail.com>
2024-03-27 21:38:38 +0000

    termino do css

--- index.php
+++ index.php
@@ -5,28 +5,30 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Oak Tecnologia</title>
-  <!--Bootstrap -->
-  <link href="css/style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
-  
-      <div class="menu">
-            <a href="?page=novo" class="itemMenu">Novo</a>
-        
-            <a href="?page=listar" class="itemMenu">Listar</a>
-</div>
-<div class="div-viewport">
-       <div class="informativo">
-        <h1>Informativos</h1>
-        Olá OAK, estou muito feliz em participar do processo seletivo, voces podem me conhecer mais um pouquinho visitando meu portifolio em:
-          Esse projeto está funcionando em: (a disponibilidade pode variar um pouco, ja que meu servidor é local)
-          Podem acompanhar do historico de desenvolvimento desse crud em ------
 
-</div> 
-  <div>
-   
-        <?php
+  <div class="menu">
+    <a href="?page=novo" class="itemMenu">Novo</a>
+
+    <a href="?page=listar" class="itemMenu">Listar</a>
+  </div>
+  <div class="informativo">
+      
+      <p>Olá equipe da OAK, estou muito feliz em participar do processo seletivo.</p>
+      <p> Vocês podem me conhecer um pouquinho mais visitando meu portfólio em: <a href="https://antonystar.com/" class="link" target="_blank">antonystar.com</a> e meu Github em: <a href="https://github.com/antonystar-dev" class="link" target="_blank">Antonystar-dev</a></p>
+      <p>Esse projeto está funcionando em: <a href="http://dev.antonystar.com/oak/" class="link" target="_blank">ServidorAS</a> (a disponibilidade pode variar um pouco, já que optei em manter no meu servidor local).
+      </p>
+      <p>Podem acompanhar o histórico de desenvolvimento desse CRUD em <a href="https://github.com/antonystar-dev/oak" class="link" target="_blank">Antonystar-dev/oak</a>.</p>
+
+    </div>
+  <div class="div-viewport">
+    
+    <div>
+
+      <?php
         include("config.php");
         switch (@$_REQUEST["page"]) {
           case "novo":
@@ -45,8 +47,8 @@
 
         }
         ?>
-      </div>
-      </div>
+    </div>
+  </div>
 </body>
 
 </html>
